077b08460dcafe420b84acdbabd2f11abad756ad
pixelbits <pixelbits100@gmail.com>
2017-05-28 22:32:23 -0600

    fix

--- build/public_api.ts
+++ build/public_api.ts
@@ -12,6 +12,7 @@
  * Entry point for all public APIs of the core package.
  */
 export * from './src/app.module';
+export { SortService } from './src/sort.service';
 
 // This file only reexports content of the `src` folder. Keep it that way.
 
